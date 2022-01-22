2179811dbe5e3d44b831c61d92e2f3a7543e25af
chechengye <262926400@qq.com>
2022-01-22 15:01:28 +0800

    注册功能简单实现

---
+++ src/day04/RegisterServlet.java
@@ -0,0 +1,77 @@
+package day04;
+
+import day02.pool.C3p0Pool;
+import day04.bean.User;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.dbutils.QueryRunner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1、处理前端发过来请求数据的中文乱码问题
+        req.setCharacterEncoding("utf-8");
+
+        /*String username = req.getParameter("username");
+        System.out.println("username = " + username);*/
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        User user = new User();
+        try {
+            //将前端传递过来的表单数组，自动映射到JavaBean对象里
+            BeanUtils.populate(user , parameterMap);
+            //数据库中若某列是存放多个值的话，   （xxx , xxx）以逗号分隔的
+            String[] hobbies = parameterMap.get("hobby");
+            String hobby = Arrays.toString(hobbies);
+            hobby = hobby.substring(1 , hobby.length() - 1);
+            //自动映射之后，不满足的属性设定，可以单独再次处理
+            user.setHobby(hobby);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        Integer rows = register(user);
+
+        if(rows > 0){//注册成功
+            System.out.println("注册成功，跳转登录~~");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req , resp);
+    }
+
+    /**
+     * 注册方法
+     * @param user
+     * @return
+     */
+    public Integer register(User user){
+
+        try {
+            QueryRunner qr = new QueryRunner(C3p0Pool.getDataSource());
+            int rows = qr.update("insert into user values(null , ? , ? ,? ,? ,? ,?)"
+                    , user.getName() , user.getUsername() , user.getPassword() , user.getSex() , user.getPhone() , user.getHobby());
+            return rows;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
---
+++ src/day04/bean/User.java
@@ -0,0 +1,90 @@
+package day04.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+    private Integer uid;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String sex;
+
+    private String hobby;
+
+    private String phone;
+
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                ", hobby='" + hobby + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
